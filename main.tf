e218f0c19aad4e8d375de9954c70967cb93b6be9
priyanshuprafful <priyanshuprafful@gmail.com>
2024-04-08 20:01:06 +0530

    updating ./tf-module-elasticache

--- main.tf
+++ main.tf
@@ -6,6 +6,9 @@ resource "aws_elasticache_cluster" "elasticache" {
   engine_version = var.engine_version
   port = var.port
   subnet_group_name = aws_elasticache_subnet_group.subnet_group.name
+
+  security_group_ids = [aws_security_group.main.id]
+
   tags = merge(
     var.tags ,
     { Name = "${var.env}-elasticache"}
